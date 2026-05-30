ce097b13530d22d93626f97ebe943b936565c12a
kashgupta01 <kashishg@bu.edu>
2026-05-30 16:39:18 -0400

    logger files

--- src/utils/logger.py
+++ src/utils/logger.py
@@ -25,6 +25,8 @@ def get_logger(name: str) -> logging.Logger:
     logger.setLevel(logging.DEBUG)
     logger.addHandler(_build_handler(sys.stdout))
 
+    #what does this line do? It adds a file handler to the logger that writes log messages to a file named "app.log" in the LOG_DIR directory. The log messages are formatted using the _fmt formatter, and the file handler is set to capture all log messages at the DEBUG level and above.
+    #will this line create app.log? Yes, if the file does not already exist, it will be created when the logger writes to it for the first time.
     file_handler = logging.FileHandler(LOG_DIR / "app.log", encoding="utf-8")
     file_handler.setFormatter(_fmt)
     file_handler.setLevel(logging.DEBUG)
