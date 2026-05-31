7b2b68e2e71b10a1de269ffd60d51d4a9a7ef959
kashgupta01 <kashishg@bu.edu>
2026-05-31 18:01:26 -0400

    Docker

--- config.py
+++ config.py
@@ -2,3 +2,5 @@ from pathlib import Path
 
 ROOT_DIR = Path(__file__).resolve().parent
 DB_PATH = ROOT_DIR / "db" / "target_knowledge.db"
+
+#All document data locations should be defined here
---
+++ src/__init__.py
@@ -0,0 +1,8 @@
+#What is the purpose of this file?
+#This file is the entry point for the package. It allows you to import the package and its modules. You can also use this file to define any global variables or functions that you want to be available when the package is imported.
+#what is generally needed in init.py?
+#In general, you will want to include the following in your __init__.py file:
+#- Import statements for any modules that you want to be available when the package is imported.
+#- Any global variables or functions that you want to be available when the package is imported.
+#- A docstring that describes the package and its purpose.
+
